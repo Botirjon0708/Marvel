2d7f386c181834f9429eb6ca62bd22328f16958d
Botirjon0708 <botirjonteshaboyev0708@gmail.com>
2023-08-28 13:14:25 +0300

    done component Charlist

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -16,7 +16,7 @@ class RandomChar extends Component {
     marvelService = new MarvelService();
     componentDidMount() {
         this.updateChar()
-        // this.timerId = setInterval(this.updateChar, 3000)
+        // this.timerId = setInterval(this.updateChar, 15000)
     }
 
     componentWillUnmount() {
@@ -27,12 +27,19 @@ class RandomChar extends Component {
         this.setState({char, loading: false})
     }
     
+    onCharLoading = () => {
+        this.setState({
+        loading: true
+    })
+}
+
     onError = () => {
         this.setState({loading: false, error: true });
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -59,7 +66,7 @@ class RandomChar extends Component {
                 Do you want to get to know him better?
                 </p>
                 <p className="randomchar__title">Or choose another one</p>
-                <button className="button button__main" onClick={() => this.updateChar()}>
+                <button onClick={this.updateChar}  className="button button__main">
                 <div className="inner">try it</div>
                 </button>
                 <img
@@ -75,14 +82,16 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const { name, description, thumbnail, homepage, wiki } = char
-    console.log(thumbnail);
+    let imgStyle = { 'objectFit': 'cover' };
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit': 'contain'}
+    }
     return (
       <div className="randomchar__block">
             <img
             src={thumbnail}
             alt="Random character"
-            className="randomchar__img"
-            />
+            className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
             <p className="randomchar__name">{name}</p>
             <p className="randomchar__descr">{description}</p>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -23,12 +23,13 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+        id: char.id,
         name: char.name,
         description: char.description ? `${char.description.slice(0,230)}...` : 'There is no description about this character',
         thumbnail:
         char.thumbnail.path + "." + char.thumbnail.extension,
         homepage: char.urls[0].url,
-        wiki: char.urls[1].url,
+        wiki: char.urls[1].url
     };
   }
 
